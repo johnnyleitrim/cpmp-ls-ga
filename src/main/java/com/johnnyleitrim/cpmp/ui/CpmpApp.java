1b76793ca98b99e7ee1a756e6100e88f7dd28e1f
John Farrelly <johnnyleitrim@gmail.com>
2021-05-13 21:13:59 +0100

    Add some useful UI elements

--- src/main/java/com/johnnyleitrim/cpmp/ui/CpmpApp.java
+++ src/main/java/com/johnnyleitrim/cpmp/ui/CpmpApp.java
@@ -13,10 +13,14 @@ import java.util.function.Function;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import com.johnnyleitrim.cpmp.Problem;
 import com.johnnyleitrim.cpmp.fitness.BFLowerBoundFitness;
@@ -31,6 +35,7 @@ public class CpmpApp {
 
   private StatePanel statePanel;
   private Problem problem;
+  private JLabel statusLine;
 
   public static void main(String[] args) {
     CpmpApp cpmpApp = new CpmpApp();
@@ -47,16 +52,20 @@ public class CpmpApp {
     solveButton.addActionListener(e -> solveProblem(problem));
     frame.add(solveButton, BorderLayout.NORTH);
 
+    statusLine = new JLabel("No problem loaded", SwingConstants.CENTER);
+    statusLine.setBorder(new BevelBorder(BevelBorder.LOWERED));
+    frame.add(statusLine, BorderLayout.SOUTH);
+
     JMenuBar menuBar = new JMenuBar();
     JMenu fileMenu = new JMenu("File");
 
     JMenuItem bfProblem = new JMenuItem("Open BF Problem");
     fileMenu.add(bfProblem);
-    addOpenFileListener(frame, bfProblem, lines -> BFProblemReader.fromLines("BF Problem", lines));
+    addOpenFileListener(frame, bfProblem, "BF Problem Files", "bay", lines -> BFProblemReader.fromLines("BF Problem", lines));
 
     JMenuItem cvProblem = new JMenuItem("Open CV Problem");
     fileMenu.add(cvProblem);
-    addOpenFileListener(frame, cvProblem, lines -> CVProblemReader.fromLines("CV Problem", lines));
+    addOpenFileListener(frame, cvProblem, "CV Problem Files", "dat", lines -> CVProblemReader.fromLines("CV Problem", lines));
 
     menuBar.add(fileMenu);
     frame.setJMenuBar(menuBar);
@@ -72,9 +81,12 @@ public class CpmpApp {
         IterativeLocalSearch cpmpSolver = new IterativeLocalSearch(problem.getInitialState(), 1, 2, new BFLowerBoundFitness(), Duration.ofMinutes(1));
         List<Move> moves = cpmpSolver.search(IterativeLocalSearch.Perturbation.LOWEST_MISOVERLAID_STACK_CLEARING, true);
         MutableState state = problem.getInitialState().copy();
-        for (Move move : moves) {
-          MoveUtils.applyMove(state, move);
-          EventQueue.invokeLater(() -> statePanel.setState(state));
+        for (int i = 0; i < moves.size(); i++) {
+          MoveUtils.applyMove(state, moves.get(i));
+          statusLine.setText(String.format("Move %d out of %d", i + 1, moves.size()));
+          EventQueue.invokeLater(() -> {
+            statePanel.setState(state);
+          });
           try {
             Thread.sleep(1000);
           } catch (InterruptedException e) {
@@ -86,13 +98,13 @@ public class CpmpApp {
     }
   }
 
-  private void addOpenFileListener(JFrame frame, JMenuItem menuItem, Function<List<String>, Problem> problemReader) {
+  private void addOpenFileListener(JFrame frame, JMenuItem menuItem, String fileDesc, String fileExt, Function<List<String>, Problem> problemReader) {
     menuItem.addActionListener(
         enterPress -> {
           JFileChooser chooser = new JFileChooser();
           chooser.setCurrentDirectory(new File("."));
-          chooser.setSelectedFile(new File(""));
-          chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+          chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+          chooser.setFileFilter(new FileNameExtensionFilter(fileDesc, fileExt));
           if (chooser.showOpenDialog(frame) == JFileChooser.OPEN_DIALOG) {
             File file = chooser.getSelectedFile();
             try {
